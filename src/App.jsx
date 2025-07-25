7b03caa2c2bd3a9d4007ce46b1151cc4cdab3ff3
elpugsito77 <diegopandacuenca9@gmail.com>
2025-07-24 21:44:37 -0600

    Añadir API key desde variables de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -134,7 +134,7 @@ export default function App() {
           }
         };
 
-        const apiKey = "";
+        const apiKey = "import.meta.env.VITE_GEMINI_API_KEY";
         const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
         const response = await fetch(apiUrl, {
